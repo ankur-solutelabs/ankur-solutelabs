109a32911d5d4fa01a6b4ca6b9600dfad278f662
ankur-solutelabs <ankur.p@solutelabs.com>
2021-12-09 17:23:24 +0530

    error handling process added

--- GitTix-app/auth/src/routes/signup.ts
+++ GitTix-app/auth/src/routes/signup.ts
@@ -16,15 +16,14 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      const error = new Error("Invalid email or password");
+      // error.reasons = errors.array();
+      throw error;
     }
-    const { email, password } = req.body;
-
     console.log("Creating a user...");
+    throw new Error("Error connecting to database");
 
     res.send({});
-
-    // new User({ email, password })
   }
 );
 
