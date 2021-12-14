b25a8a8eb1b9400f2c48101066eac3b2ca621246
ankur-solutelabs <ankur.p@solutelabs.com>
2021-12-14 15:52:43 +0530

    common library is ready to use

--- GitTix-app/auth/src/routes/current-user.ts
+++ GitTix-app/auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@ankurpandey131/common';
 
 const router = express.Router();
 
