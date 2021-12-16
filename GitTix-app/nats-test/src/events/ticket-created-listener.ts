a1e940144ad994f066309ab4370055f266116940
ankur-solutelabs <ankur.p@solutelabs.com>
2021-12-16 16:09:23 +0530

    readonly

--- GitTix-app/nats-test/src/events/ticket-created-listener.ts
+++ GitTix-app/nats-test/src/events/ticket-created-listener.ts
@@ -4,6 +4,7 @@ import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  //readonly works too
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
