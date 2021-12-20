70afb5b43c39d58922c8ce7a624d6110104dd644
ankur-solutelabs <ankur.p@solutelabs.com>
2021-12-20 15:22:13 +0530

    ticket created and updated listeners added on event and manual test

--- GitTix-app/orders/src/index.ts
+++ GitTix-app/orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -32,18 +34,18 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
     });
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+  app.listen(3030, () => {
+    console.log('Listening on port 3030');
   });
 };
 
